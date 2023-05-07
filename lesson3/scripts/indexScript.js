cd4d2d04508166d15c118497119cabd319625f6d
Rexcreamer <byuajw@gmail.com>
2023-05-06 21:15:18 -0700

    ...

---
+++ lesson3/scripts/indexScript.js
@@ -0,0 +1,16 @@
+let dateCha = document.getElementById("datetod");
+let dateYear = document.getElementById("yearnow");
+
+const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+};
+
+const yearOptions = {
+    year: "numeric",
+};
+
+dateCha.textContent = new Date().toLocaleDateString("en-US", options);
+dateYear.textContent = new Date().toLocaleDateString("en-US", yearOptions);
